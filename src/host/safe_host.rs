3a8ac4a8b17e8bf4a6dc0c6ff48d394156cae909
Greg Fraley <gsfraley@gmail.com>
2017-10-10 16:49:06 -0400

    Finished safe host wrapper functions (untested)

--- src/host/safe_host.rs
+++ src/host/safe_host.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
 use std::ffi::CString;
 use std::os::raw::{c_char, c_int, c_uint, c_void};
+use std::path::Path;
 
-use super::unsafe_host::{coreclr_initialize, coreclr_shutdown};
+use super::unsafe_host::{
+    coreclr_create_delegate, coreclr_execute_assembly,
+    coreclr_initialize,
+    coreclr_shutdown, coreclr_shutdown_2};
 
 fn to_c_str<T: Into<Vec<u8>>>(t: T) -> *const c_char {
     CString::new(t).unwrap().as_ptr() as *const c_char
@@ -11,6 +15,10 @@ fn to_c_str<T: Into<Vec<u8>>>(t: T) -> *const c_char {
 pub struct CoreClr {
     host_handle: *const c_void,
     domain_id: c_uint
+}
+
+pub struct CoreClrDelegate {
+    pub delegate_handle: *const c_void
 }
 
 impl CoreClr {
@@ -44,16 +52,17 @@ impl CoreClr {
         let properties_values_ref = &properties_values[0] as *const *const c_char;
 
         unsafe {
-            coreclr_initialize(
+            assert_eq!(
+                coreclr_initialize(
                 exe_path_raw,
                 app_domain_friendly_name_raw,
                 properties_count,
                 properties_keys_ref,
                 properties_values_ref,
-                host_handle_ref, domain_id_ref);
+                host_handle_ref, domain_id_ref), 0);
         }
 
-        return CoreClr {
+        CoreClr {
             host_handle: host_handle,
             domain_id: domain_id
         }
@@ -61,9 +70,71 @@ impl CoreClr {
 
     pub fn shutdown(self: Self) {
         unsafe {
-            coreclr_shutdown(
-                self.host_handle,
-                self.domain_id);
+            assert_eq!(
+                coreclr_shutdown(
+                    self.host_handle,
+                    self.domain_id), 0);
         }
+    }
+
+    pub fn shutdown_2(self: Self) -> c_int {
+        let latched_exit_code = 0 as c_int;
+        let latched_exit_code_ref = &latched_exit_code as *const c_int;
+
+        unsafe {
+            assert_eq!(
+                coreclr_shutdown_2(
+                    self.host_handle,
+                    self.domain_id,
+                    latched_exit_code_ref), 0);
+        }
+
+        latched_exit_code
+    }
+    
+    pub fn create_delegate(self: &Self) -> CoreClrDelegate {
+        let delegate_handle = 0 as *const c_void;
+        let delegate_handle_ref = &delegate_handle as *const *const c_void;
+
+        unsafe {
+            assert_eq!(
+                coreclr_create_delegate(
+                    self.host_handle as *const c_void,
+                    self.domain_id as c_uint,
+                    0 as *const c_char,
+                    0 as *const c_char,
+                    0 as *const c_char,
+                    delegate_handle_ref), 0);
+        }
+
+        CoreClrDelegate {
+            delegate_handle: delegate_handle
+        }
+    }
+
+    pub fn execute_assembly(self: &Self, args: Vec<&str>, managed_assembly_path: &Path) -> c_uint {
+        let exit_code = 0 as c_uint;
+        let exit_code_ref = &exit_code as *const c_uint;
+
+        let args_count = args.len() as c_int;
+        let args_raw: Vec<*const c_char> = args.iter()
+            .map(|&a| to_c_str(a))
+            .collect();
+        
+        let args_raw_ref = &args_raw[0] as *const *const c_char;
+        let managed_assembly_path: *const c_char = to_c_str(managed_assembly_path.to_str().unwrap());
+
+        unsafe {
+            assert_eq!(
+                coreclr_execute_assembly(
+                    self.host_handle,
+                    self.domain_id,
+                    args_count,
+                    args_raw_ref,
+                    managed_assembly_path,
+                    exit_code_ref), 0);
+        }
+
+        exit_code
     }
 }
