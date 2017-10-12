ae65abd22a62b434448f87b20a78a2385a9567c6
Greg Fraley <gsfraley@gmail.com>
2017-10-12 16:02:59 -0400

    Consolidated hosts into single host module

---
+++ src/host.rs
@@ -0,0 +1,73 @@
+extern crate libloading as libl;
+
+use std::collections::HashMap;
+use std::ffi::CString;
+use std::os::raw::{c_char, c_int, c_uint, c_void};
+
+fn to_c_str<T: Into<Vec<u8>>>(t: T) -> *const c_char {
+    CString::new(t).unwrap().as_ptr() as *const c_char
+}
+
+pub struct CoreClr {
+    host_handle: *const c_void,
+    domain_id: c_uint
+}
+
+pub type CoreClrInitializeFn = unsafe extern fn(
+    *const c_char,
+    *const c_char,
+    c_int,
+    *const *const c_char,
+    *const *const c_char,
+    *const *const c_void,
+    *const c_uint) -> c_int;
+
+pub fn coreclr_initialize(
+    exe_path: &str,
+    app_domain_friendly_name: &str,
+    properties_option: Option<HashMap<&str, &str>>) -> libl::Result<CoreClr>
+{
+    let host_handle = 0 as *const c_void;
+    let host_handle_ref = &host_handle as *const *const c_void;
+    
+    let domain_id = 0 as c_uint;
+    let domain_id_ref = &domain_id as *const c_uint;
+
+    let exe_path_raw = to_c_str(exe_path);
+    let app_domain_friendly_name_raw = to_c_str(app_domain_friendly_name);
+
+    let properties = properties_option.unwrap_or_else(HashMap::new);
+    let properties_count = properties.len() as c_int;
+
+    let properties_keys: Vec<*const c_char> = properties.keys()
+            .map(|&k| to_c_str(k))
+            .collect();
+
+    let properties_values: Vec<*const c_char> = properties.values()
+            .map(|&v| to_c_str(v))
+            .collect();
+
+    let properties_keys_ref = &properties_keys[0] as *const *const c_char;
+    let properties_values_ref = &properties_values[0] as *const *const c_char;
+
+    let coreclr_library = libl::Library::new("/usr/local/share/dotnet/shared/Microsoft.NETCore.App/2.0.0")?;
+    
+    unsafe {
+        let coreclr_initialize_fn: libl::Symbol<CoreClrInitializeFn> = coreclr_library.get(b"coreclr_initialize")?;
+
+        assert_eq!(
+            coreclr_initialize_fn(
+                exe_path_raw,
+                app_domain_friendly_name_raw,
+                properties_count,
+                properties_keys_ref,
+                properties_values_ref,
+                host_handle_ref,
+                domain_id_ref), 0);
+    }
+
+    Ok(CoreClr {
+        host_handle: host_handle,
+        domain_id: domain_id
+    })
+}
--- src/host/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod unsafe_host;
-pub mod safe_host;
